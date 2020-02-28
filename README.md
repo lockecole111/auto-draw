e4ecb741cb4ea48706028c0194c6a32129b08a70
lockecole111 <lockecole111@gmail.com>
2020-02-29 06:54:37 +0800

    1

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # auto-draw
 自动 "你画我猜" 游戏辅助工具。
+![](../image/demo.png)
  
 Thanks to the following projects:
 
 
  **[linedraw](https://github.com/LingDong-/linedraw)**
-
-
  **pynput**
 
--- api.py
+++ api.py
@@ -7,6 +7,7 @@ import json
 import random
 import string
 import time
+import os
 
 def getImg(url):
     filename =  "images/%s"%(''.join(random.sample(string.ascii_letters + string.digits, 16)))
@@ -15,22 +16,24 @@ def getImg(url):
     with open(filename, "wb") as code: 
         code.write(data)
     return filename
+def delImg(filename):
+    if(os.path.exists(filename)):
+        os.remove(filename)
+    
 
 def drawKey(key):
-    #key = str(key)
+
     api = 'http://image.so.com/j?q=%s&src=srp&correct=&sn=1&pn='%key
-    print api
     req = urllib2.urlopen(api)
-    print req
     res = req.read()
-    print res
     res = json.loads(res)
     res = res['list'][0]['thumb']
     filename = getImg(res)
-#    time.sleep(3)
     if filename:
         i = draw.ImageDrawer(480,280)
         i.drawImage(filename)
+    delImg(filename)
+    
 
     
 if __name__ == '__main__':
